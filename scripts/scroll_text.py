4d54eb95dc15fc2a62e96c02d973c4d23c662d16
sternenseemann <git@lukasepple.de>
2016-11-06 01:00:48 +0100

    scroll_text: render frames in advance
    Previously, the script just waited for 1/FPS s before rendering the next
    frame and immediately displaying it. So the assumption is that rendering
    the next frame takes zero seconds. This however is not even close enough,
    since the script is deployed on a Raspberry Pi for flipdot-gschichtler,
    where rendering a frame sometimes takes a bit longer. This leads to very
    unpleasant frame rate incosistencies.
    
    This is fixed by having one thread fill the frame buffer and another one
    sending the next frame every 1/FPS seconds.

--- scripts/scroll_text.py
+++ scripts/scroll_text.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python2
+# -.- coding: utf-8
 
 import socket, time
 from PIL import Image, ImageFont, ImageDraw
 import sys
 import fileinput
+import threading
 
 UDPHOST="flipdot.openlab.lan"
 UDPPORT=2323
@@ -19,6 +21,9 @@ FONT_OFFSET= (1, -1)
 C_BLACK = (0, 0, 0)
 C_WHITE = (255, 255, 255)
 
+finished = False
+framebuffer = []
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 def list2byte(l):
@@ -32,8 +37,7 @@ def array2packet(a):
     return str(bytearray([list2byte(a[i*8:i*8+8]) for i in range(len(a)/8)]))
 
 def str2array(s):
-    #font = ImageFont.load_default()
-    font = ImageFont.truetype("/usr/share/fonts/TTF/DejaVuSans-Bold.ttf", FONT_SIZE)
+    font = ImageFont.load_default()
 
     img_width = font.getsize(s)[0]
     image     = Image.new("RGBA", (img_width,DISPLAY_SIZE[1]), C_BLACK)
@@ -51,8 +55,13 @@ def str2array(s):
             imgmap.append(1 if INVERT else 0)
     return imgmap
 
+def render_frames(imgmap):
+    global framebuffer
+    global finished
 
-def scroll_text(imgmap):
+    framebuffer = []
+
+    finished = False
 
     display_width  = DISPLAY_SIZE[0]
     display_heigth = DISPLAY_SIZE[1]
@@ -61,30 +70,56 @@ def scroll_text(imgmap):
     scrollimg_width = imgmap_width + 2*display_width
 
     scroll_imgmap = [1 if INVERT else 0] * (scrollimg_width * display_heigth)
-    
+
     # expand imgmap
     for row in range(display_heigth):
         for col in range(imgmap_width):
             scroll_imgmap[display_width-STEPS + row*scrollimg_width + col] = imgmap[col+row*imgmap_width]
 
-
     # scroll img
     curr_frame = [0] * display_width * display_heigth
-    
+
     for offset in range(0, scrollimg_width-display_width, STEPS):
         for row in range(display_heigth):
             for col in range(display_width):
                     curr_frame[col+row*display_width] = scroll_imgmap[col+row*scrollimg_width + offset]
-        
-        sock.sendto(array2packet(curr_frame), (UDPHOST, UDPPORT))
-        time.sleep(1.0/FPS)
 
-try:        
+        framebuffer.append(array2packet(curr_frame))
+
+    finished = True
+
+
+def send_frames():
+    global framebuffer
+    global finished
+    frame_pos = 0
+
+    def frames_left(frame_pos, framebuffer):
+        return frame_pos < len(framebuffer)
+
+    while not finished or frames_left(frame_pos, framebuffer):
+        if frames_left(frame_pos, framebuffer):
+            sock.sendto(framebuffer[frame_pos], (UDPHOST, UDPPORT))
+            time.sleep(1.0/FPS)
+            frame_pos += 1
+        else:
+            continue
+
+try:
     while True:
-        text = sys.stdin.readline().decode('utf-8')
+        text = sys.stdin.readline().decode('utf-8').strip()
         if text == "":
             break
-        scroll_text(str2array(text.strip()))
+        imgmap = str2array(text)
+
+        renderer = threading.Thread(target=render_frames,args=(imgmap,))
+        sender = threading.Thread(target=send_frames)
+
+        renderer.start()
+        sender.start()
+
+        sender.join()
+
 except KeyboardInterrupt:
-    print("")
+    print("Exited due to Ctrl-C")
     sys.exit(0)
